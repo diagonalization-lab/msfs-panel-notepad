003c705f0b3a772b583f66eff4c0707ff0e2694f
Diagonalization <diagonalization@fastmail.com>
2021-07-01 11:45:02 -0400

    USe fixed-width font for notepad

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 "%MSFS_SDK%\Tools\bin\fspackagetool.exe" package.xml
-xcopy /s /e .\Packages\diagonalization-panel-notepad "%userprofile%\AppData\Local\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Community\"
+xcopy /e .\Packages\diagonalization-panel-notepad "%USERPROFILE%\AppData\Local\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\Packages\Community\diagonalization-panel-notepad"
